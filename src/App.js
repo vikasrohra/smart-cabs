070c1d6c41f3be6d3c6aa97ca91b391b4c1ab520
vikas rohra <vikas.rohra@impressico.local>
2022-09-16 00:49:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api';
+//useJsApiLoader, A hook to tell weather maps script is loaded on the browser or not
+//Where we have to show the Google Map
 import './App.css';
+import Temp from './components/temp';
+
+const CENTER = { lat: 48.8584, lng: 2.2945 };
+const LIBRARIES = ['places'];
 
 function App() {
+  const [translateSearchCabsArrow, setTranslateSearchCabsArrow] = useState(false);
+  const [map, setMap] = useState(/** @type google.maps.Map */ null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState('');
+  const [duration, setDuration] = useState('');
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const pickupRef = useRef();
+  const dropRef = useRef();
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyDJlbhYyDTemG0tYpyObolvcmjdkrmt3w4',
+    // libraries: [...LIBRARIES],
+  });
+
+  const handleTranslateSearchCabsArrow = (isTranslate) => {
+    setTranslateSearchCabsArrow(isTranslate);
+  }
+
+  const calculateRoute = async () => {
+    debugger;
+    if (pickupRef.current.value === '' || dropRef.current.value === '') {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: pickupRef.current.value,
+      destination: dropRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING
+    });
+    setDirectionsResponse(results);
+    console.log(results.routes[0].legs[0].distance.text); // set this value in state
+    console.log(results.routes[0].legs[0].duration.text); // set this value in state
+  }
+
+  const clearRoute = () => {
+    setDirectionsResponse(null);
+    setDistance('');
+    setDuration('');
+    pickupRef.origin.value = '';
+    dropRef.origin.value = '';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Skeleton until map is loaded */}
+      {!isLoaded &&
+        <div className="shadow rounded-md p-4 h-[100vh]">
+          <div className="animate-pulse flex space-x-4">
+            <div className="flex-1 space-y-6 py-1">
+              <div className="h-2 bg-slate-700 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+                </div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+
+      {/* Google Map Section */}
+      {isLoaded &&
+        <section>
+          {/* <Temp Autocomplete={Autocomplete} translateSearchCabsArrow={translateSearchCabsArrow} handleTranslateSearchCabsArrow={handleTranslateSearchCabsArrow} pickupRef={pickupRef} dropRef={dropRef} calculateRoute={calculateRoute} /> */}
+          <GoogleMap
+            center={CENTER}
+            zoom={15}
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            // onLoad={map => setMap(map)} 
+            >
+            <Marker position={CENTER} />
+            {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+          </GoogleMap>
+        </section>
+      }
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    // themes: ["dark", "light"],
+    //     themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
